10048eb010ec1dfda95de898bd7d995e5d56f4c0
bba3477 <117315487+bba3477@users.noreply.github.com>
2023-05-04 16:47:02 +0200

    Add files via upload

--- AmoucheChamsedine_AhmedGaloul_SAE_Graphes.py
+++ AmoucheChamsedine_AhmedGaloul_SAE_Graphes.py
@@ -3,26 +3,26 @@ def pref(u):
         longueur = len(u)
         lst_pref = [u[:i] for i in range(longueur + 1)]
         return lst_pref
-    else:
+    else: # Si u n'est pas une chaine alors on affiche une erreur
         print("Erreur, le paramètre n'est pas une chaîne de caractère.")
 
 def suf(u):
     if type(u) == str: # Vérifie si le paramètre est une chaîne de caractère.
         lst = []
         for i in range(len(u)+1):
-            lst.append(u[i:len(u)])
+            lst.append(u[i:len(u)]) # slice qui permet d'ajouter les suffixe en enlenvant une lettre a chaque fois
         return lst
-    else:
+    else: # Si u n'est pas une chaine alors on affiche une erreur
         print("Erreur, le paramètre n'est pas une chaîne de caractère.")
 
 def fact(u):
     if type(u) == str: # Vérifie si le paramètre est une chaîne de caractère.
-        facteurs = set() # Set pour ne éviter les doublons
+        facteurs = set() # Set pour éviter les doublons
         for i in range(len(u)):
             for j in range(i, len(u)+1):
-                facteurs.add(u[i:j])
+                facteurs.add(u[i:j]) # ajoute la sous-chaîne de u allant de i à j inclus dans facteurs
         return sorted(list(facteurs))
-    else:
+    else :# Si u n'est pas une chaine alors on affiche une erreur
         print("Erreur, le paramètre n'est pas une chaîne de caractère.")
 
 def miroir(u):
@@ -32,6 +32,12 @@ def miroir(u):
         print("Erreur, le paramètre n'est pas une chaîne de cractère.")
 
 if __name__ == "__main__":
+    print("Préfixe")
     print(pref('coucou'))
-    print(miroir('coucou'))
+    print("\n Suffixe")
+    print(suf('coucou'))
+    print("\n Facteur")
     print(fact('coucou'))
+    print("\n Miroir")
+    print(miroir('coucou'))
+    
