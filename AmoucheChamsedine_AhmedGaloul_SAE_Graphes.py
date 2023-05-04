7d77525776a2b340b4be6b09e837c8d05e2c4adb
bba3477 <117315487+bba3477@users.noreply.github.com>
2023-05-04 16:41:33 +0200

    Add files via upload

--- AmoucheChamsedine_AhmedGaloul_SAE_Graphes.py
+++ AmoucheChamsedine_AhmedGaloul_SAE_Graphes.py
@@ -1,11 +1,29 @@
 def pref(u):
-    if type(u) == str : #Vérifie si le paramètre est une chaîne de cractère.
+    if type(u) == str: # Vérifie si le paramètre est une chaîne de caractère.
         longueur = len(u)
         lst_pref = [u[:i] for i in range(longueur + 1)]
         return lst_pref
-    else: #Si le paramètre n'est pas uen chaîne de caractère.
-        print("Erreur, le paramètre n'est pas une chaîne de cractère.")
+    else:
+        print("Erreur, le paramètre n'est pas une chaîne de caractère.")
 
+def suf(u):
+    if type(u) == str: # Vérifie si le paramètre est une chaîne de caractère.
+        lst = []
+        for i in range(len(u)+1):
+            lst.append(u[i:len(u)])
+        return lst
+    else:
+        print("Erreur, le paramètre n'est pas une chaîne de caractère.")
+
+def fact(u):
+    if type(u) == str: # Vérifie si le paramètre est une chaîne de caractère.
+        facteurs = set() # Set pour ne éviter les doublons
+        for i in range(len(u)):
+            for j in range(i, len(u)+1):
+                facteurs.add(u[i:j])
+        return sorted(list(facteurs))
+    else:
+        print("Erreur, le paramètre n'est pas une chaîne de caractère.")
 
 def miroir(u):
     if type(u) == str : #Vérifie si le paramètre est une chaîne de cractère.
@@ -16,3 +34,4 @@ def miroir(u):
 if __name__ == "__main__":
     print(pref('coucou'))
     print(miroir('coucou'))
+    print(fact('coucou'))
